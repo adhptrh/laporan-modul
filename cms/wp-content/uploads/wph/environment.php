c619cceda5ffe2817df4f5b934cb3bde6172256b
adhptrh <70842975+adhptrh@users.noreply.github.com>
2021-10-21 15:00:59 +0700

    commit

---
+++ cms/wp-content/uploads/wph/environment.php
@@ -0,0 +1,2 @@
+<?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
+$environment_variable = '{"theme":{"folder_name":"blankslate","mapped_name":""},"allowed_paths":["E:\/xampp\/htdocs\/laporan-modul\/cms\/wp-content\/themes"],"cache_path":"E:\/xampp\/htdocs\/laporan-modul\/cms\/wp-content\/cache\/wph\/","wordpress_directory":"","site_relative_path":"\/laporan-modul\/cms\/"}'; ?>
